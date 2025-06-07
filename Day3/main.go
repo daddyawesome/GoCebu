cc49724254b22a5e25c7a7e62c896eb434aa8229
Mark Russel Miranda <45071051+daddyawesome@users.noreply.github.com>
2025-06-07 07:31:32 +0000

    Adding route

--- Day3/main.go
+++ Day3/main.go
@@ -6,17 +6,22 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Go Web Development!")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the about page")
+	fmt.Fprintln(w, "This is the About Page.")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is the Contact Page.")
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
