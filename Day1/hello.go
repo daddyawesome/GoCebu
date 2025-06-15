46d0d1e3901b34aae1da4f60da360bb961178cb9
Mark Russel Miranda <45071051+daddyawesome@users.noreply.github.com>
2025-06-15 12:39:39 +0000

    Conditionals, Loops, and Structs

--- Day1/hello.go
+++ Day1/hello.go
@@ -2,9 +2,27 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name string
+	Age  int
+}
+
 func main() {
-    var name string
-    fmt.Print("Enter your name: ")
-    fmt.Scan(&name)
-    fmt.Println("Welcome to Go,", name)
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Println("Welcome to Go,", name)
+
+	users := []User{
+		{"Alice", 17},
+		{"Bob", 22},
+	}
+
+	for _, user := range users {
+		if user.Age >= 18 {
+			fmt.Println(user.Name, "is an Adult")
+		} else {
+			fmt.Println(user.Name, "is a Minor")
+		}
+	}
 }
